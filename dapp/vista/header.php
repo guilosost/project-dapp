cd8e62f3dbe243c19593d674730bebb3f99951ac
guilosost <48485791+guilosost@users.noreply.github.com>
2019-03-31 20:03:04 +0200

    hkjhb

---
+++ dapp/vista/header.php
@@ -0,0 +1,5 @@
+<header>
+<img class="logo-izq" src='../../vista/img/logo-dapp70x70..jpeg' alt="logo de DAPP">
+<img class= "logo-dch" src='../../vista/img/logo-dapp70x70.jpeg' alt= "logo de DAPP">
+<p class="title">DAPP - PUBLICACIÓN MÚLTIPLE</p> 
+</header>
