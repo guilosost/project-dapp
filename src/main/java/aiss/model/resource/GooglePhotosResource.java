141edaf61605f27db191f626185b87139dad8c9a
migyanari <48485652+migyanari@users.noreply.github.com>
2019-04-25 00:22:28 +0200

    f

--- src/main/java/aiss/model/resource/GooglePhotosResource.java
+++ src/main/java/aiss/model/resource/GooglePhotosResource.java
@@ -1,6 +1,13 @@
 package aiss.model.resource;
 
+import java.util.Map;
 import java.util.logging.Logger;
+
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import aiss.model.google.drive.FileItem;
+import aiss.model.googlephotos.MediaItem;
 
 // https://photoslibrary.googleapis.com/v1/mediaItems
 public class GooglePhotosResource {
@@ -12,5 +19,25 @@ public class GooglePhotosResource {
 	public GooglePhotosResource(String access_token) {
 		this.access_token = access_token;
 	}
+	
+	public String insertFile(MediaItem file, String content) {
+		
+		
+
+        ClientResource cr = null;
+        String newId = null;
+        try {
+            cr = new ClientResource(uri + "?access_token=" + access_token);
+            MediaItem newFile = cr.post(file, MediaItem.class);
+            newId = newFile.getId();
+            cr = new ClientResource(uri_upload + "/" + newId + "?uploadType=media&access_token=" + access_token);
+            Map<String, Object> headers = cr.getRequestAttributes();
+            headers.put("Content-Type", "text/plain");
+            cr.put(content);
+        } catch (ResourceException re) {
+            log.warning("Error when inserting file: " + cr.getResponse().getStatus());
+        }
+        return newId;
+    }
 
 }
