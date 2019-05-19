099d0003dc1801b8bc5ec089700df3f36801d53d
migyanari <48485652+migyanari@users.noreply.github.com>
2019-05-19 04:54:30 +0200

    t

---
+++ src/test/java/aiss/model/resources/DeviantArtTest.java
@@ -0,0 +1,70 @@
+package aiss.model.resources;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
+import aiss.model.deviantart.DeviantArtUser;
+import aiss.model.deviantart.GetFolderByID;
+import aiss.model.deviantart.DeviantArtResult;
+import aiss.model.deviantart.SearchDeviantArt;
+import aiss.model.resource.DeviantArtResource;
+
+
+public class DeviantArtTest {
+
+	@Test
+	public void getPhotosTest() throws UnsupportedEncodingException {
+		String query = "photo";
+		DeviantArtResource datos = new DeviantArtResource("c87b3d9a2bfe08babe97bed3d241cba6b00ba8caf216a2efa5");
+		
+		//Search
+		SearchDeviantArt daSearch = datos.getDeviantArtImages(query);
+		
+		assertNotNull("The search returned null", daSearch);
+		assertNotNull("The search returned null", daSearch.getResults());
+		assertFalse("The number of photos of " + query + " is zero", daSearch.getResults().size() == 0);
+		
+		System.out.println(
+		"The search for " + query + "'s photos returned " + daSearch.getResults().size()
+		+ " files.");
+		
+		for(DeviantArtResult r : daSearch.getResults()) {
+			System.out.println(r.getUrl());
+		}
+		
+		//User
+		DeviantArtUser user = datos.getDeviantArtUserInfo();
+		
+		assertNotNull("The search returned null", user);
+		assertNotNull("The search returned null", user.getUser());
+		
+		System.out.println("Usuario: " + user.getUser().getUsername());
+		
+		//Stats
+		SearchDeviantArt stats = datos.getDeviantArtStats();
+		
+		assertNotNull("The search returned null", stats);
+		assertNotNull("The search returned null", stats.getResults());
+		assertFalse("The number of user photos is zero", stats.getResults().size() == 0);
+		
+		System.out.println("El usuario tiene " + stats.getResults().size() + " fotos");
+		for(DeviantArtResult r : stats.getResults()) {
+			System.out.println(r.getTitle() + "\n");
+		}
+		
+		//Favs
+		GetFolderByID folder = datos.getDeviantArtFavs();
+		assertNotNull("The search returned null", folder);
+		assertNotNull("The search returned null", folder.getResults());
+		assertFalse("The number of user photos is zero", folder.getResults().size() == 0);
+		
+		for(DeviantArtResult r : folder.getResults()) {
+			System.out.println(r.getDeviationid());
+		}
+		
+	}
+}
---
+++ src/test/java/aiss/model/resources/YoutubeTest.java
@@ -0,0 +1,32 @@
+package aiss.model.resources;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
+import aiss.model.resource.YoutubeResource;
+import aiss.model.youtube.Item;
+import aiss.model.youtube.YoutubeSearch;
+
+public class YoutubeTest {
+	@Test
+	public void getDailymotionTest() throws UnsupportedEncodingException {
+		String query = "star wars";
+		YoutubeResource datos = new YoutubeResource("ya29.GlsOB7HR9F-ZNmaLqvHXUJgxYbO4t2iAj7I0-F18i1lWlJBaveomY5VtH2IdJQj1AT9YFGeFvms1R772-3dFJ4O1vqnri7fmkGbzLTVi00_izcGmMw7b-obN8uxa");
+		YoutubeSearch dailySearch = datos.searchYoutubeVideos(query);
+
+		assertNotNull("The search returned null", dailySearch);
+		assertNotNull("The search returned null", dailySearch.getItems());
+		assertFalse("The number of videos of " + query + " is zero", dailySearch.getItems().size() == 0);
+
+		System.out.println("The search for " + query + "'s videos returned " + dailySearch.getItems().size() + " datas.");
+
+		for (Item l : dailySearch.getItems()) {
+			System.out.println(l.getSnippet().getTitle());
+		}
+	}
+
+}
