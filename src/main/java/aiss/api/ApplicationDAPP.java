9a19d94715c4969aa543e26b83e7ffe2a54b61e8
migyanari <48485652+migyanari@users.noreply.github.com>
2019-05-26 18:24:30 +0200

    DOC

--- src/main/java/aiss/api/ApplicationDAPP.java
+++ src/main/java/aiss/api/ApplicationDAPP.java
@@ -5,9 +5,7 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
-import aiss.api.resources.ImageResource;
 import aiss.api.resources.MultimediaResource;
-import aiss.api.resources.VideoResource;
 
 public class ApplicationDAPP extends Application {
 	private Set<Object> singletons = new HashSet<Object>();
@@ -16,8 +14,6 @@ public class ApplicationDAPP extends Application {
 	// Loads all resources that are implemented in the application
 	// so that they can be found by RESTEasy
 	public ApplicationDAPP() {
-		singletons.add(ImageResource.getInstance());
-		singletons.add(VideoResource.getInstance());
 		singletons.add(MultimediaResource.getInstance());
 	}
 
